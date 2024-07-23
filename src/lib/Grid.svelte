76430c8962543f93a81a8cc8a1f9d234d16368f5
marcoce005 <marcoce005@gmail.com>
2024-07-23 18:31:54 +0200

    cellini: connect to Firebase DB test

--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -1,35 +1,34 @@
 <script>
-    import {
-	Table,
-	TableBody,
-	TableBodyCell,
-	TableBodyRow,
-	TableHead,
-	TableHeadCell,
-    } from "flowbite-svelte";
-    import { AwardSolid } from 'flowbite-svelte-icons';
+  import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from "flowbite-svelte";
+  import { AwardSolid } from "flowbite-svelte-icons";
 
-    const table_padding = "px-4 py-6 w-1/6";
-    const name_width = "w-3/5";
-    const points_width = "w-1/4";
+  const table_padding = "px-4 py-6 w-1/6";
+  const name_width = "w-3/5";
+  const points_width = "w-1/4";
 
-    
-    function get_prize_color(pos) {
-	const first_prize_color = "#ffd700";
-	const second_prize_color = "#949494";
-	const third_prize_color = "#cd7f32";
-	switch(pos) {
-	    case 0:
-		return first_prize_color;
-	    case 1:
-		return second_prize_color;
-	    case 2:
-		return third_prize_color;
-	    default:
-		break;
-	}
+  function get_prize_color(pos) {
+    const first_prize_color = "#ffd700";
+    const second_prize_color = "#949494";
+    const third_prize_color = "#cd7f32";
+    switch (pos) {
+      case 0:
+        return first_prize_color;
+      case 1:
+        return second_prize_color;
+      case 2:
+        return third_prize_color;
+      default:
+        break;
     }
-    export let items;
+  }
+  export let items;
 </script>
 
 <!-- 
@@ -53,17 +52,17 @@
   -->
 
 <svelte:head>
-    <style>
-	/* Nello style di Svelte questo stile viene rimosso in quanto 
+  <style>
+    /* Nello style di Svelte questo stile viene rimosso in quanto 
 	   non usato nella pagina ma nel componente TableBodyCell.
 	   Come workaround lo metto nel HTML
 	*/
-	svg {
-	    display: block;
-	    text-align: center;
-	    margin: auto;
-	}
-    </style>
+    svg {
+      display: block;
+      text-align: center;
+      margin: auto;
+    }
+  </style>
 </svelte:head>
 
 <div class="lg:flex lg:justify-center">
@@ -78,27 +77,29 @@
         {#each items as item, i}
           <TableBodyRow>
             <TableBodyCell tdClass={table_padding} class="text-center">
-	    {#if i >= 0 && i <= 2}
-		<AwardSolid size="xl" color="{get_prize_color(i)}"></AwardSolid>
-	    {:else}
-		<div class="text-base gamer-font text-4xl">{i + 1}</div>
-	    {/if}
+              {#if i >= 0 && i <= 2}
+                <AwardSolid size="xl" color={get_prize_color(i)}></AwardSolid>
+              {:else}
+                <div class="text-base gamer-font text-4xl">{i + 1}</div>
+              {/if}
             </TableBodyCell>
             <TableBodyCell
               class="flex items-center px-6 py-4 text-gray-900 dark:text-white {name_width} space-x-4"
             >
               <img
-		class="lg:w-2/6 w-3/6 rounded-full"
-                src={item.image}
-                alt="{item.name} avatar"
+                class="lg:w-2/6 w-3/6 rounded-full"
+                src={item.avatar}
+                alt="{item.username} avatar"
               />
               <div class="ps-3 pr-6">
-                <div class="text-base gamer-font text-4xl pr-6">{item.name}</div>
+                <div class="text-base gamer-font text-4xl pr-6">
+                  {item.username}
+                </div>
               </div>
             </TableBodyCell>
             <TableBodyCell class={points_width}>
-		<div class="text-base gamer-font text-4xl">{item.points}</div>
-	    </TableBodyCell>
+              <div class="text-base gamer-font text-4xl">{item.points}</div>
+            </TableBodyCell>
           </TableBodyRow>
         {/each}
       </TableBody>
@@ -106,26 +107,25 @@
   </div>
 </div>
 
-
 <style>
-    @font-face {
-	font-family: "Knight-Warrior";
-	src: url(/Knight-Warrior-BF6527a6f3a30ed.otf);
+  @font-face {
+    font-family: "Knight-Warrior";
+    src: url(/Knight-Warrior-BF6527a6f3a30ed.otf);
+  }
+
+  /* Computer */
+  @media screen and (min-width: 720px) {
+    .gamer-font {
+      font-family: "Knight-Warrior";
+      font-size: xx-large;
     }
-    
-    /* Computer */
-    @media screen and (min-width: 720px) {
-      .gamer-font {
-	font-family: "Knight-Warrior";
-	font-size: xx-large;
-      }
+  }
+
+  /* Dispositivi mobili */
+  @media screen and (max-width: 500px) {
+    .gamer-font {
+      font-family: "Knight-Warrior";
+      font-size: large;
     }
-    
-    /* Dispositivi mobili */
-    @media screen and (max-width: 500px) {
-      .gamer-font {
-	font-family: "Knight-Warrior";
-	font-size: large;
-      }
-    }
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,87 @@
 <script>
-    import Grid from "../lib/Grid.svelte";
-    const example_items = [
-	{
-	  name: "Linus Torvalds",
-	  points: 5000,
-	  image: "/user_placeholder.png",
-	},
-	{
-	  name: "RMS",
-	  points: 4000,
-	  image: "/user_placeholder.png",
-	},
-	{
-	  name: "Ken Thompson",
-	  points: 3000,
-	  image: "/user_placeholder.png",
-	},
-	{
-	  name: "Pippo Pluto",
-	  points: 2000,
-	  image: "/user_placeholder.png",
-	},
-	{
-	  name: "Ciro Esplosito",
-	  points: 1500,
-	  image: "/user_placeholder.png",
-	},
-	{
-	  name: "Pippo Pluto",
-	  points: 1000,
-	  image: "/user_placeholder.png",
-	},
-     ];
+	import Grid from "../lib/Grid.svelte";
+
+	import { initializeApp } from "firebase/app";
+    import { getDatabase, child, get, ref, set } from "firebase/database";
+    import { onMount } from "svelte";
+
+	const firebaseConfig = {
+        apiKey: "AIzaSyDMmEc92Gi0791mVLMl5UcuDTuUxy5ettw",
+        authDomain: "prova-6bba2.firebaseapp.com",
+        databaseURL:
+            "https://prova-6bba2-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "prova-6bba2",
+        storageBucket: "prova-6bba2.appspot.com",
+        messagingSenderId: "310664797099",
+        appId: "1:310664797099:web:6edb3661b14d1bfc6450ee",
+        measurementId: "G-HH7RTEYHWY",
+    };
+	const app = initializeApp(firebaseConfig);
+
+	const readData = async () => {
+        const dbRef = ref(getDatabase());
+        const get_db = await get(child(dbRef, `leaderboard/`));
+        return get_db.val();
+    };
+
+	const update_table = async (mat, classe) => {
+        if (mat === "all") {
+            let all_subjetcs = Object.values(full_db.TPSI).concat(
+                Object.values(full_db.reti),
+                Object.values(full_db.informatica),
+            ).filter((e) => e.class == classe);
+
+            leaderboard = Array.from(
+                new Set(all_subjetcs.map((e) => e.username)),
+            )
+                .map((e) => all_subjetcs.filter((el) => el.username === e))
+                .map((e) => {
+                    return {
+                        username: e[0].username,
+                        avatar: e[0].avatar,
+                        class: e[0].class,
+                        points: e.reduce((acc, e) => acc + e.points, 0),
+                    };
+                })
+                .sort((a, b) => b.points - a.points);
+        } else {
+            let l = full_db[`${mat}`];
+            leaderboard = Object.values(l)
+                .sort((a, b) => b.points - a.points)
+                .filter((e) => e.class == classe);
+        }
+    };
+
+    onMount(async () => {
+        /* default mount is with data about 3 info subjet: TPSI */
+        full_db = await readData();
+        update_table("TPSI", 3);
+    });
+	
+	let leaderboard = [], classe, subject, full_db = {};
 </script>
 
-<Grid items={example_items}></Grid>
+<select
+    name="classe"
+    id="classe"
+    bind:value={classe}
+    on:change={update_table(subject, classe)}
+>
+    <option value="3">3 Info</option>
+    <option value="4">4 Info</option>
+    <option value="5">5 Info</option>
+</select>
+
+<select
+    name="materia"
+    id="materia"
+    bind:value={subject}
+    on:change={update_table(subject, classe)}
+>
+    <option value="TPSI">TPSI</option>
+    <option value="informatica">Informatica</option>
+    <option value="reti">Sistemi e Reti</option>
+    <option value="all">Totale</option>
+</select>
+
+<Grid items={leaderboard}></Grid>
