e0cb7e232551efff9d316b589a8c117177681736
marcoce005 <marcoce005@gmail.com>
2024-07-31 12:08:25 +0200

    cellini: new database (test)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,68 @@
 <script>
     import Grid from "../lib/Grid.svelte";
-
-    import { initializeApp } from "firebase/app";
-    import { getDatabase, child, get, ref } from "firebase/database";
-    import { onMount } from "svelte";
     import Select from "../lib/Select.svelte";
     import Label from "../lib/Label.svelte";
 
+    import { initializeApp } from "firebase/app";
+    import {
+        getFirestore,
+        collection,
+        query,
+        where,
+        getDocs,
+    } from "firebase/firestore";
+    import { onMount } from "svelte";
+
     const firebaseConfig = {
-        apiKey: "AIzaSyBv2SwNbM94fs7et1ts6aClTXreO5a6zDM",
-        authDomain: "lambsoffame.firebaseapp.com",
+        apiKey: "AIzaSyDMmEc92Gi0791mVLMl5UcuDTuUxy5ettw",
+        authDomain: "prova-6bba2.firebaseapp.com",
         databaseURL:
-            "https://lambsoffame-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "lambsoffame",
-        storageBucket: "lambsoffame.appspot.com",
-        messagingSenderId: "583547239362",
-        appId: "1:583547239362:web:eda69c76496ccc9cbd345b",
+            "https://prova-6bba2-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "prova-6bba2",
+        storageBucket: "prova-6bba2.appspot.com",
+        messagingSenderId: "310664797099",
+        appId: "1:310664797099:web:cfcea80eb318b2a16450ee",
+        measurementId: "G-0RFXQ1M1T5",
     };
+
     const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
-    const readData = async () => {
-        const dbRef = ref(getDatabase());
-        const get_db = await get(child(dbRef, `leaderboard/`));
-        return get_db.val();
+    const calAnnoScolastico = (date) => {
+        return Number(date[0]) > 6
+            ? `${date[1]}-${String(Number(date[1]) + 1)}`
+            : `${String(Number(date[1]) - 1)}-${date[1]}`;
     };
 
-    const sum_subject_points = (classe) => {
-        let all_subjetcs = Object.values(full_db.TPSI)
-            .concat(
-                Object.values(full_db.reti),
-                Object.values(full_db.informatica),
-            )
-            .filter((e) => e.class == classe);
+    const getQueriesFiltered = async (sub, as, classe) => {
+        const col = collection(db, "leaderboard", "toQuery", "queries");
+        let q;
 
-        leaderboard = Array.from(new Set(all_subjetcs.map((e) => e.username)))
-            .map((e) => all_subjetcs.filter((el) => el.username === e))
+        if (!sub && !as && !classe) {
+            q = query(col);
+        } else if (!sub) {
+            q = query(col, where("as", "==", as), where("class", "==", classe));
+        } else {
+            q = query(
+                col,
+                where("subject", "==", sub),
+                where("as", "==", as),
+                where("class", "==", classe),
+            );
+        }
+
+        const querySnapshot = await getDocs(q);
+        let l = querySnapshot.docs.map((doc) => doc.data());
+        console.log();
+        return adjustForTable(l);
+    };
+
+    const adjustForTable = (list) => {
+        return Array.from(new Set(list.map((e) => e.username)))
+            .map((e) => list.filter((el) => el.username === e))
             .map((e) => {
                 return {
                     username: e[0].username,
-                    avatar: e[0].avatar,
-                    class: e[0].class,
                     points: e.reduce((acc, e) => acc + e.points, 0),
                 };
             })
@@ -49,80 +72,116 @@
     const update_table = async (mat, classe) => {
         switch (mat) {
             case "sum-subjects":
-                sum_subject_points(classe);
+                leaderboard = await getQueriesFiltered(
+                    undefined,
+                    calAnnoScolastico(
+                        new Date()
+                            .toLocaleString("it-IT")
+                            .split(",")[0]
+                            .split("/")
+                            .slice(1),
+                    ),
+                    classe,
+                );
                 break;
 
             case "all":
-                leaderboard = Object.values(full_db["generale"]).sort(
-                    (a, b) => b.points - a.points,
+                leaderboard = await getQueriesFiltered(
+                    undefined,
+                    undefined,
+                    undefined,
                 );
                 break;
             default:
-                let l = full_db[`${mat}`];
-                leaderboard = Object.values(l)
-                    .sort((a, b) => b.points - a.points)
-                    .filter((e) => e.class == classe);
+                leaderboard = await getQueriesFiltered(
+                    mat,
+                    calAnnoScolastico(
+                        new Date()
+                            .toLocaleString("it-IT")
+                            .split(",")[0]
+                            .split("/")
+                            .slice(1),
+                    ),
+                    classe,
+                );
                 break;
         }
     };
 
     onMount(async () => {
-        /* default mount is with data about 3 info subjet: TPSI */
-        full_db = await readData();
-        update_table("TPSI", 3);
+        leaderboard = await getQueriesFiltered(
+            "TPSI",
+            calAnnoScolastico(
+                new Date()
+                    .toLocaleString("it-IT")
+                    .split(",")[0]
+                    .split("/")
+                    .slice(1),
+            ),
+            3,
+        );
     });
 
     let leaderboard = [],
         classe,
-        subject,
-        full_db = {};
-    
+        subject;
+
     const classes = [
         {
             value: 3,
-            text: "3 INFO"
+            text: "3 INFO",
         },
         {
             value: 4,
-            text: "4 INFO"
+            text: "4 INFO",
         },
         {
             value: 5,
-            text: "5 INFO"
+            text: "5 INFO",
         },
     ];
     const subjects = [
         {
             value: "TPSI",
-            text: "TPSI"
+            text: "TPSI",
         },
         {
             value: "informatica",
-            text: "Informatica"
+            text: "Informatica",
         },
         {
             value: "reti",
-            text: "Sistemi e Reti"
+            text: "Sistemi e Reti",
         },
         {
             value: "sum-subjects",
-            text: "Totale materie"
+            text: "Totale materie",
         },
         {
             value: "all",
-            text: "Generale"
-        }
+            text: "Generale",
+        },
     ];
 </script>
 
-<div class="select-container flex justify-between max-w-screen-sm rounded-xl m-auto p-5 bg-slate-50 ">
+<div
+    class="select-container flex justify-between max-w-screen-sm rounded-xl m-auto p-5 bg-slate-50"
+>
     <div>
         <Label parent="select-class" text="Seleziona la classe:"></Label>
-        <Select options={classes} bind:value={classe} on_change={() => update_table(subject, classe)} />
+        <Select
+            options={classes}
+            bind:value={classe}
+            on_change={() => update_table(subject, classe)}
+        />
     </div>
     <div>
         <Label parent="select-subject" text="Seleziona la materia:"></Label>
-        <Select options={subjects} bind:value={subject} on_change={() => update_table(subject, classe)}/>
+        <Select
+            options={subjects}
+            bind:value={subject}
+            on_change={() => update_table(subject, classe)}
+        />
     </div>
 </div>
 
